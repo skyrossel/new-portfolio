589f5b915bab9c033ff2c32c2ce4bdb7827bd19c
SOEmil <149309639+SOEmil@users.noreply.github.com>
2025-01-08 15:26:34 +0500

    first commit

--- components/noise-background.tsx
+++ components/noise-background.tsx
@@ -67,7 +67,7 @@ const NoiseBackground = () => {
   return (
     <canvas
       id="noise-background"
-      className="pointer-events-none fixed inset-0 z-[-1] hidden md:block"
+      className="pointer-events-none fixed inset-0 z-[-1]"
     />
   );
 };
